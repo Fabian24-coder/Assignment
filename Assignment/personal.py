1e8d892c33f5f1c8fc898f1a113309049b47bc64
Fabian24-coder <fabianckson@gmail.com>
2026-09-15 16:51:03 +0300

    Initial commit

---
+++ Assignment/personal.py
@@ -0,0 +1,9 @@
+myName = "Alioma Franco Buni"
+myAge = 13
+myHeight = 123
+isStudent = True
+
+print(f"Name: {myName}")
+print(f"Age: {myAge} Years old")
+print(f"Height: {myHeight}centimeters")
+print(f"Student status: {isStudent }")
